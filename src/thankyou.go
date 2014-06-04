e1ab9e1096e5e6ea2384b1891ffb255f18158cca
Kletsun Yuri <klec@speroteck.com>
2014-06-04 18:20:05 +0300

    refactoring split Page and App

--- src/thankyou.go
+++ src/thankyou.go
@@ -9,24 +9,22 @@ import (
 	"os"
 	"strings"
 	"labix.org/v2/mgo/bson"
-	"net"
 	"strconv"
 )
 
 
 func main() {
-	var app = new(App)
-	//app.init()
+	var App = new(Application)
+	App.GetMongoConnection() //@todo
 	http.Handle("/static/", http.FileServer(http.Dir("./")))
-//	http.HandleFunc("/addpers", app.addPersonal)
-	http.HandleFunc("/index.html", app.handll)
+	//http.HandleFunc("/addpers", app.addPersonal)
+	http.HandleFunc("/index.html", App.handll)
 	//http.Post("/addreview", app.addreview)
 	http.ListenAndServe(":8080",nil)
 }
 
-type App struct {
-
-	layout *template.Template
+type Application struct {
+	DbSource *mgo.Database
 }
 
 type Person struct {
@@ -39,7 +37,7 @@ type Review struct {
 	Slave		int
 	Action		string
 	Master		int
-	MasterIp 	net.IP
+	MasterIp 	string
 }
 
 type Template struct {
@@ -50,18 +48,14 @@ type Template struct {
 type Page struct {
 	Best1, Best2, Best3	string
 	Loosers			map[int]string
+	layout			*template.Template
+	SlavesOptions	template.HTML
 }
 
-func (p *Page)init() {
-	connection:=GetMongoConnection()
-
-
+func (a *Application)getReviews()  {
 	pipeline:=[]bson.M{
 		bson.M{"$group":
-			bson.M{
-				"_id": "$level",
-				"summ":	bson.M{"$sum": 1,},
-			},
+			bson.M{	"_id": "$level","summ":	bson.M{"$sum": 1,},	},
 		},
 		bson.M{"$sort":
 			bson.M{ "summ": -1},
@@ -79,7 +73,7 @@ func (p *Page)init() {
 		Ok     bool
 	}{}
 
-	err := connection.Run(query, &answer)
+	err := a.DbSource.Run(query, &answer)
 	if nil!=err {
 		fmt.Println( err)
 	}
@@ -88,39 +82,35 @@ func (p *Page)init() {
 
 	}
 
+	//return answer.Result
 }
 
-func (a *App) handll(writer http.ResponseWriter, request *http.Request) {
+func (a *Application) handll(writer http.ResponseWriter, request *http.Request) {
 	if (request.Method == "POST") {
-		a.addReview(request)
-
-
+//		a.addReview(request)
 	}
-	a.render(writer)
+	//var reviews = []Review{}
+	//a.getReviews()
+
+	slaves:=a.GetSlaves()
+	page := &Page{"первый","второй","третий", nil, nil, template.HTML("")}
+	fmt.Println(slaves)
+	page.Render(writer)
 }
 
-func (a *App) addReview(request *http.Request){
+func (a *Application) addReview(request *http.Request){
 	slave, _  := strconv.Atoi(request.FormValue("slave"))
 	action := request.FormValue("action")
 	master, _ := strconv.Atoi(request.FormValue("master"))
-	connection:=GetMongoConnection()
-	review:=&Review{Slave:slave, Action:action, Master: master, MasterIp: net.IPv4(127,0,0,1)}
-	connection.C("reviews").Insert(review)
+	review:=&Review{Slave:slave, Action:action, Master: master, MasterIp: request.RemoteAddr}
+	a.DbSource.C("reviews").Insert(review) //@todo add creation time
+	//@todo add slave notification
+
 }
 
-func (a *App)render(writer http.ResponseWriter) {
-	layout, err := template.ParseFiles("answer.html")
-	if (err != nil) {fmt.Println(err)}
-	a.layout = layout
-	page := &Page{"первый","второй","третий", nil}
-	page.init();
-	err = a.layout.ExecuteTemplate(writer, "answer", page)
-	if nil!=err {fmt.Println( err)}
-}
 
-func (a *App)addPersonal(writer http.ResponseWriter, request *http.Request){
-	connection:=GetMongoConnection()
-	fmt.Println(connection)
+func (a *Application) addPersonal(writer http.ResponseWriter, request *http.Request){
+	//fmt.Println(connection)
 
 	file, _ := os.Open("personal.html")
 	scanner := bufio.NewScanner(file)
@@ -130,31 +120,38 @@ func (a *App)addPersonal(writer http.ResponseWriter, request *http.Request){
 		row:=scanner.Text()
 		roww := strings.Split(row, "\t")
 		p:=&Person{ID: i, Name: roww[0], Email: roww[1]}
-		//connection.C("persons").Insert(p)
+//		a.DbSource.C("persons").Insert(p)
 		fmt.Println(p)
 	}
 }
 
-func (p *Page)GetSlaves() template.HTML{
+func (a *Application)GetSlaves() template.HTML{
 	html := "<option>Имя нашего героя</option>"
-	connection:=GetMongoConnection()
 	res:=[]Person{}
-	iter := connection.C("persons").Find(nil).Iter()
+	iter := a.DbSource.C("persons").Find(nil).Iter()
 	err:=iter.All(&res)
 	if(err!=nil){fmt.Println(err)}
 	for i:=range res{
 		person := res[i]
 		//fmt.Println(person.Name)
-		html+="<option value="+strconv.Itoa(i)+">"+person.Name+"</option>"
+		html+="<option value=\""+strconv.Itoa(i)+"\" >"+person.Name+"</option>"
 	}
-	html+="</option>"
 	return template.HTML(html)
 }
 
-func GetMongoConnection() *mgo.Database {
+func (p *Page)Render(writer http.ResponseWriter) {
+	layout, err := template.ParseFiles("answer.html")
+	if (err != nil) {fmt.Println(err)}
+	p.layout = layout
+
+	err = p.layout.ExecuteTemplate(writer, "answer", p)
+	if nil!=err {fmt.Println( err)}
+}
+
+func (a *Application) GetMongoConnection() *mgo.Database {
 	connection, err := mgo.Dial("localhost")
 	if(err!=nil){fmt.Println(err)}
 	db := connection.DB("thanks")
-
+	a.DbSource = db
 	return db;
 }
