2dc81bf4095f57cedb105f0a21fd6263f71b5a74
Kletsun Yuri <klec@speroteck.com>
2014-06-04 22:39:07 +0300

    real data from db working copy

--- src/thankyou.go
+++ src/thankyou.go
@@ -38,6 +38,7 @@ type Review struct {
 	Action		string
 	Master		int
 	MasterIp 	string
+	Time		string
 }
 
 type Template struct {
@@ -47,29 +48,36 @@ type Template struct {
 
 type Page struct {
 	Best1, Best2, Best3	string
-	Loosers			map[int]string
+	Loosers			[8]string
 	layout			*template.Template
 	SlavesOptions	template.HTML
 }
 
-func (a *Application)getReviews()  {
-	pipeline:=[]bson.M{
-		bson.M{"$group":
-			bson.M{	"_id": "$level","summ":	bson.M{"$sum": 1,},	},
-		},
-		bson.M{"$sort":
-			bson.M{ "summ": -1},
-		},
-
-	}
-
+func (a *Application)getReviews() [8]string {
 	query := bson.D{
-		{"aggregate","templates"},
-		{"pipeline", pipeline},
+		{"aggregate","reviews"},
+		{"pipeline", []bson.M{
+			bson.M{"$sort":
+				bson.M{ "action": 1},
+			},
+
+			bson.M{"$group":
+				bson.M{	"_id": "$slave",
+					"summ":	bson.M{"$sum": 1},
+					"slave": bson.M{"$first":"$slave"},
+					"action":bson.M{"$first":"$action"},
+	//				"masterip":bson.M{"$first":"$masterip"},
+				},
+			},
+			bson.M{"$sort":
+				bson.M{ "summ": -1},
+			},
+			bson.M{"$limit":8 },
+		}},
 	}
 
 	answer := struct {
-		Result []map[string] int
+		Result []map[string] interface {}
 		Ok     bool
 	}{}
 
@@ -77,24 +85,30 @@ func (a *Application)getReviews()  {
 	if nil!=err {
 		fmt.Println( err)
 	}
+	var Reviews = [8]string{}
+	var lastTemplate []string
 	for row:= range answer.Result {
-		fmt.Println(answer.Result[row])
+		action := answer.Result[row]["action"].(string)
 
+		template := a.GetTemplate(answer.Result[row]["summ"].(int), lastTemplate)
+		lastTemplate = append(lastTemplate, template)
+		person := a.GetSlave(answer.Result[row]["slave"].(int))
+		oneReview:= strings.Replace(template, "Он", person, -1)
+		oneReview = strings.Replace(oneReview, "Помог", action, -1)
+
+		Reviews[row]=oneReview
 	}
-
-	//return answer.Result
+	return Reviews
 }
 
 func (a *Application) handll(writer http.ResponseWriter, request *http.Request) {
 	if (request.Method == "POST") {
-//		a.addReview(request)
+		a.addReview(request)
 	}
-	//var reviews = []Review{}
-	//a.getReviews()
+	loosers:=a.getReviews()
 
 	slaves:=a.GetSlaves()
-	page := &Page{"первый","второй","третий", nil, nil, template.HTML("")}
-	fmt.Println(slaves)
+	page := &Page{loosers[0], loosers[1], loosers[2], loosers, nil, slaves}
 	page.Render(writer)
 }
 
@@ -110,8 +124,6 @@ func (a *Application) addReview(request *http.Request){
 
 
 func (a *Application) addPersonal(writer http.ResponseWriter, request *http.Request){
-	//fmt.Println(connection)
-
 	file, _ := os.Open("personal.html")
 	scanner := bufio.NewScanner(file)
 	i :=0
@@ -125,18 +137,46 @@ func (a *Application) addPersonal(writer http.ResponseWriter, request *http.Requ
 	}
 }
 
+func (a *Application)GetSlave(id int) string{
+	res:=[]Person{}
+	iter := a.DbSource.C("persons").Find(bson.M{"id":id})
+	err:=iter.All(&res)
+	if(err!=nil){fmt.Println(err)}
+	p:=res[0]
+	return p.Name
+}
+func (a *Application)GetTemplate(id int, ne []string) string{
+	res:=[]Template{}
+	iter := a.DbSource.C("templates").Find(bson.M{"level":id, "body":bson.M{"$nin":ne}})
+	err:=iter.All(&res)
+	if(err!=nil){fmt.Println(err)}
+	t:=res[0]
+	return t.Body
+}
+
 func (a *Application)GetSlaves() template.HTML{
 	html := "<option>Имя нашего героя</option>"
 	res:=[]Person{}
-	iter := a.DbSource.C("persons").Find(nil).Iter()
+	iter := a.DbSource.C("persons").Find(nil).Sort("id").Iter()
 	err:=iter.All(&res)
 	if(err!=nil){fmt.Println(err)}
 	for i:=range res{
 		person := res[i]
-		//fmt.Println(person.Name)
-		html+="<option value=\""+strconv.Itoa(i)+"\" >"+person.Name+"</option>"
+		html+="<option value=\""+strconv.Itoa(person.ID)+"\" >"+person.Name+"</option>"
 	}
 	return template.HTML(html)
+}
+
+func (p *Page)GetLoosers() template.HTML{
+	html:=""
+	for i:=range p.Loosers{
+		if(i>2){
+			person := p.Loosers[i]
+			html+="<li>"+person+"</li>\n"
+		}
+	}
+	return template.HTML(html)
+
 }
 
 func (p *Page)Render(writer http.ResponseWriter) {
